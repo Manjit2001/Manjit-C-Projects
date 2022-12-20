ed9bb9a5edd6ab5fa3b23f70770f234aed36fcde
Manjit2001 <120300707+Manjit2001@users.noreply.github.com>
2022-12-20 12:46:45 +0530

    Create fact

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void add
+main()
+{
+printf("\n welcome manjit");
+  printf("\n Aissms Coe");
+  add();
+  
+}
+void add()
+{
+  int a,b,c;
+  printf("\n enter the values of a and b");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n add=%d",c);
