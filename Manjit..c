b9945661cfcbb13dec4cd3bcbd6a48a6c29b4df5
Manjit2001 <120300707+Manjit2001@users.noreply.github.com>
2022-12-20 12:04:36 +0530

    Update Manjit.c

--- Manjit..c
+++ Manjit..c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 main()
 {
-printf("\n welcome manjiut");
+printf("\n welcome manjit");
+  printf("\n Aissms Coe");
+  add();
+  
 }
+void add()
+{
+  int a,b,c;
+  printf("\n enter the values of a and b");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n add=%d",c);
